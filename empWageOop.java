93875f17a92573ee473b93d88bc2610e07147c2e
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-27 11:25:12 +0530

    Added method for getting employee hours

--- empWageOop.java
+++ empWageOop.java
@@ -2,15 +2,35 @@ public class empWageOop {
 
 	public static int getEmpAttendance () {
 
-		int attendance = (int) (Math.floor(Math.random () * 10 ) % 3);
-		System.out.println("Attendance" + attendance);
+		int attendance = (int) (Math.floor (Math.random () * 10 ) % 3);
+		System.out.println ("Attendance" + attendance);
 		return attendance;
+
+	}
+
+	public static int getEmpHours () {
+
+		int empHours = 0;
+		switch (getEmpAttendance()) {
+
+			case 1:
+				System.out.println ("Employee is Full Time");
+				empHours = 8;
+				break;
+			case 2:
+				System.out.println ("Employee is Part Time");
+				empHours = 4;
+				break;
+			default:
+				System.out.println ("Employee is Absent");
+		}
+		return empHours;
 
 	}
 
 	public static void main (String[] args) {
 
-		System.out.println("Welcome to employee wage builder using OOP");
-		getEmpAttendance();
+		System.out.println ("Welcome to employee wage builder using OOP");
+		getEmpHours ();
 	}
 }
